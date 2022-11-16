6a44b9f1db7cc67422782c7773ad0f44a8697ab6
enieves <enieves@106-29.info2>
2022-11-16 14:33:46 +0100

    t3

--- EjerciciosT3-EloyNieves/src/ejercicios/Ejercicio07.java
+++ EjerciciosT3-EloyNieves/src/ejercicios/Ejercicio07.java
@@ -7,7 +7,7 @@ public class Ejercicio07 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int num = 0, line = 0, repetir = 0, progresion = 0;
+		int num = 0, line = 0, repetir = 0, progresion = 0, numm=0;
 		System.out.println("pon un numero");
 		// numero de filas
 		num = sc.nextInt();
