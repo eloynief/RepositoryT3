644378498503834800830f11b85b533273d8df70
enieves <enieves@106-29.info2>
2022-11-16 14:31:28 +0100

    t3

---
+++ EjerciciosT3-EloyNieves/src/ejercicios/Ejercicio07.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = 0, line = 0, repetir = 0, progresion = 0;
+		System.out.println("pon un numero");
+		// numero de filas
+		num = sc.nextInt();
+
+		while (num > 0 && num < 20) {
+			/* Comando for el cual aumentará el numero de líneas */
+			for (line = 1; line <= num; line++) {
+				/* Este comando hace que los números se incrementan */
+				for (repetir = 0; repetir < line; repetir++) {
+					System.out.print(line);
+				}
+				// este print servirá para aumentar las líneas
+				System.out.println();
+			}
+			break;
+
+		}
+	}
+
+}
